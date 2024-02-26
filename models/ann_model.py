33c0f86b03a7cd200e0ea3dec14cd3d846cf24c8
Dayman556 <cormacduffy648@hotmail.com>
2024-02-26 05:17:46 +0000

    initial ann model

--- models/ann_model.py
+++ models/ann_model.py
@@ -1,10 +1,43 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
 
 #Loading the Data 
 df = pd.read_csv('estimated_average_selling_prices_fuel_sources (1).csv')
 
 print(df.head())
+
+features = ['Year', 'coalprice', 'oilprice', 'gasprice', 'nuclearprice', 'hydroprice', 'windsolarprice', 'cokebreezeprice']
+X = df[features]  # Your input features
+y = df['avgprice']  # Your target variable
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 32)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Initialising the ANN
+model = Sequential([
+    Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
+    Dense(64, activation='relu'),
+    Dense(1, activation='linear')
+])
+
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+model.fit(X_train, y_train, validation_split=0.2, epochs=500, batch_size=32)
+
+model.evaluate(X_test, y_test)
+
+predictions = model.predict(X_test)
+
+test_loss, test_mae = model.evaluate(X_test, y_test)
+print(f"Test Loss: {test_loss}, Test MAE: {test_mae}")
